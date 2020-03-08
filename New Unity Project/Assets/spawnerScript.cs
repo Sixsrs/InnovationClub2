e7ed4acd2a28020b8e1eef3472db3df42d7ec6ea
Sixsrs <41872687+Sixsrs@users.noreply.github.com>
2020-03-08 17:12:59 -0400

    Start to Enemy Spawner

---
+++ New Unity Project/Assets/spawnerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerScript : MonoBehaviour
+{
+    int timer = 0;
+    public Object enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1;
+        if ((timer % 30) == 0)
+        {
+            Instantiate(enemy, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        }
+        
+    }
+}
