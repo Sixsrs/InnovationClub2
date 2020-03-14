6885cc40f71a8cb49ebea46d63c3164de020fac8
Sixsrs <41872687+Sixsrs@users.noreply.github.com>
2020-03-14 10:38:53 -0400

    Improvements to Spawning
    Also added some random stuff into the game just to mess with the spawner.

--- New Unity Project/Assets/spawnerScript.cs
+++ New Unity Project/Assets/spawnerScript.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class spawnerScript : MonoBehaviour
 {
+    public int maxCapacity = 10;
+    public int tenSecondSpawn = 5;
+    public bool spawnWhenPlayerPresent = false;
     int timer = 0;
+    int capacity = 0;
     public Object enemy;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("pixel-8x8");
+        //PlayerScript playerScript = player.GetComponent<PlayerScript>();
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         timer += 1;
-        if ((timer % 30) == 0)
+        if ((((timer % Mathf.Floor(300/tenSecondSpawn)) == 0) && capacity < maxCapacity) &&
+           ((spawnWhenPlayerPresent) || 
+           (player.transform.position.x+9f < (transform.position.x - transform.localScale.x / 2f)) ||
+           (player.transform.position.x-9f > (transform.position.x + transform.localScale.x / 2f)) ||
+           (player.transform.position.y+5.5f < (transform.position.y - transform.localScale.y / 2f)) ||
+           (player.transform.position.y-5.5f > (transform.position.y + transform.localScale.y / 2f))
+
+
+           ))
         {
+            capacity++;
             Instantiate(enemy, new Vector3(Random.Range(transform.position.x - transform.localScale.x/2f, transform.position.x + transform.localScale.x/2f), Random.Range(transform.position.y - transform.localScale.y / 2f, transform.position.y + transform.localScale.y / 2f), 0f), Quaternion.identity);
         }
         
