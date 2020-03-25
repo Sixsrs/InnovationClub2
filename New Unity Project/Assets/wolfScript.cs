e667024dd39945d6984635b22bf30c56817de849
Sixsrs <41872687+Sixsrs@users.noreply.github.com>
2020-03-25 18:10:41 -0400

    change

--- New Unity Project/Assets/wolfScript.cs
+++ New Unity Project/Assets/wolfScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class wolfScript : MonoBehaviour
 {
-    public int sightRadius = 10;
-    public int attackRadius = 7;
-    public int stopRadius = 6;
+    public float sightRadius = 10;
+    public float attackRadius = 7;
+    public float stopRadius = 4;
     public float speed = .03f;
     int attackTimer = 0;
     public GameObject player;
     float magnitude;
+    int reset;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,30 +23,32 @@ public class wolfScript : MonoBehaviour
     void FixedUpdate()
     {
         magnitude = Mathf.Pow(Mathf.Pow(player.transform.position.x - transform.position.x, 2) + Mathf.Pow(player.transform.position.y - transform.position.y, 2f), .5f);
-        if (magnitude < sightRadius)
+        if ((magnitude < sightRadius) && (magnitude > stopRadius))
         {
+            print(magnitude);
             transform.position = new Vector2(transform.position.x + (player.transform.position.x - transform.position.x) * speed / magnitude, transform.position.y + (player.transform.position.y - transform.position.y) * speed / magnitude);
-        }
-        if (magnitude < attackRadius)
-        {
 
-            
-            if (attackTimer == 0)
+            if (magnitude < attackRadius)
             {
-                attackTimer = 195;
-                speed = 0f;
+
+
+                if (attackTimer == reset)
+                {
+                    attackTimer = 195;
+                    reset = Random.Range(-50, 50);
+                    speed = 0f;
+                }
+                else if (attackTimer == 180)
+                {
+                    speed = .12f;
+                }
+                else if (attackTimer == 120)
+                {
+                    speed = .06f;
+                }
+                attackTimer--;
             }
-            else if (attackTimer == 180)
-            {
-                speed = .12f;
-            }
-            else if (attackTimer == 120)
-            {
-                speed = .06f;
-            }
-            attackTimer--;
+
         }
-        
-        
     }
 }
